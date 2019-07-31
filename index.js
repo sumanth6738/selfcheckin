a13f36d24b1eb19139a916fd7c10b66aab4c7dd2
sumanth <sumanth6738@gmail.com>
2019-07-31 09:29:15 +0530

    index file changes

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const port = 3005
 const { mongoose } = require('./config/database')
 const cors = require('cors')
+const path = require('path')  //c
 
 const app = express()
 
-const {visitorController} = require ('./app/controllers/visitorController')
+const { visitorController } = require('./app/controllers/visitorController')
 const { meetController } = require('./app/controllers/meetingController')
-const {adminController} = require('./app/controllers/adminController')
+const { adminController } = require('./app/controllers/adminController')
 app.use(cors())
 app.use(express.json())
 
@@ -16,6 +17,7 @@ app.use('/wantToMeet', meetController)
 app.use('/admin', adminController)
 
 app.use('/assets/uploads', express.static(__dirname + '/assets/uploads'));
+app.use(express.static(path.join(__dirname, "client/build")))   //c
 
 app.listen(port, () => {
     console.log('connected to port ', port)
