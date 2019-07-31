60dd4fa60420c73597d808cf97669e5bde3a0dd2
sumanth <sumanth6738@gmail.com>
2019-07-31 11:39:26 +0530

    heroku changes

--- config/database.js
+++ config/database.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost:27017/mverve_self_checkin', { useNewUrlParser: true })
-.then(function(){
-    console.log('connected to db')
-})
-.catch(function(err){
-    console.log('something went wrong in DB connetion')
-})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mverve_self_checkin', { useNewUrlParser: true })
+    .then(function () {
+        console.log('connected to db')
+    })
+    .catch(function (err) {
+        console.log('something went wrong in DB connetion')
+    })
 
 module.exports = {
     mongoose
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const port = 3005
+const port = process.env.port || 3005
 const { mongoose } = require('./config/database')
 const cors = require('cors')
-const path = require('path')  //c
+const path = require('path')
+
 
 const app = express()
 
@@ -17,8 +18,13 @@ app.use('/wantToMeet', meetController)
 app.use('/admin', adminController)
 
 app.use('/assets/uploads', express.static(__dirname + '/assets/uploads'));
-app.use(express.static(path.join(__dirname, "client/build")))   //c
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 app.listen(port, () => {
     console.log('connected to port ', port)
 })
